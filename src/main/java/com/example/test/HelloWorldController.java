0a90909188e03828833228482faa997b9390e3f9
Mouna Wahada~ <@mounawahada4>
2023-11-23 20:22:04 +0100

    Initial commit

--- src/main/java/com/example/test/HelloWorldController.java
+++ src/main/java/com/example/test/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
     // GET HTTP Method
-// http://localhost:8080/hello-world
+// http://localhost:8082/hello-world
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World!";
---
+++ src/main/java/com/example/test/StudentController.java
@@ -0,0 +1,20 @@
+package com.example.test;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.test.Student;
+
+@RestController
+public class StudentController {
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Ramesh", "Fadatare"));
+        students.add(new Student("Tony", "Cena"));
+        students.add(new Student("Sanjay", "Jadhav"));
+        students.add(new Student("Ram", "Jadhav"));
+        students.add(new Student("Umesh", "Fadatare"));
+        return students;
+    }
+}
